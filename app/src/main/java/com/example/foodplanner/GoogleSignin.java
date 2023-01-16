520750ce738b3a93a6f263db9044391669016cfe
eman <emankhaledsha@gmail.com>
2023-01-16 12:22:04 +0200

    edit Google sign in

--- app/src/main/java/com/example/foodplanner/GoogleSignin.java
+++ app/src/main/java/com/example/foodplanner/GoogleSignin.java
@@ -1,26 +1,93 @@
 package com.example.foodplanner;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import static android.content.ContentValues.TAG;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.widget.Toast;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class GoogleSignin extends LoginActivity {
-        private static final int RC_SIGN_IN = 101;
-  GoogleSignInClient googleSignInClient;
+    private static final int RC_SIGN_IN = 101;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    GoogleSignInClient googleSignInClient;
+    ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       GoogleSignInOptions googleSignInOptions= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
-     googleSignInClient=GoogleSignin.getClient(this, googleSignInOptions);
-        Intent signInIntent= googleSignInClient.getSignInIntent();
-       startActivityForResult(signInIntent, RC_SIGN_IN);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Google sign in...");
+        progressDialog.show();
 
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
 
-   }
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                //finish();
+                Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+                finish();
+
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Log.d(TAG, "signInWithCredential: success");
+                        progressDialog.dismiss();
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        //finish();
+                        progressDialog.dismiss();
+                        Toast.makeText(GoogleSignin.this, "" + task.getException(), Toast.LENGTH_SHORT).show();
+                        updateUI(null);
+                        finish();
+
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        Intent intent = new Intent(GoogleSignin.this, HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+}
 
 
