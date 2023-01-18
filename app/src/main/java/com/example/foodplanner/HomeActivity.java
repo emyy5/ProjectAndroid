588c4aa6667675229b8e6206c4369b846a77401e
Huda <Hudakamal6>
2023-01-18 14:03:36 -0800

    Home and regiester Xml

--- app/src/main/java/com/example/foodplanner/HomeActivity.java
+++ app/src/main/java/com/example/foodplanner/HomeActivity.java
@@ -1,17 +1,112 @@
 package com.example.foodplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class HomeActivity extends AppCompatActivity {
+    private static Retrofit retrofit;
+    RecyclerView recyclerView;
+    MealAdapter adapter;
+
+
+    // ui for random meal
+    TextView txt;
+    TextView txt2;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home2);
+        setContentView(R.layout.activity_home);
+        recyclerView = findViewById(R.id.recyclerView);
+        txt = findViewById(R.id.mealName);
+        txt2 = findViewById(R.id.mealId);
+        image = findViewById(R.id.image);
 
-        Toast.makeText(this, "Welcome in HomeActivity", Toast.LENGTH_SHORT).show();
+
+        Retrofit apiClient= APIClient.getClient();
+
+        APIinterface apiInterface =  apiClient.create( APIinterface.class);
+
+        Call<RootMeal> call = apiInterface.getRandomMeals();
+
+
+        call.enqueue(new Callback<RootMeal>() {
+            @Override
+            public void onResponse(Call<RootMeal> call, Response<RootMeal> response) {
+
+                String imgURl = response.body().getMeals().get(0).getStrMealThumb();
+
+                Log.i("img", "onResponse:"+imgURl);
+                Glide.with(image.getContext()).load(imgURl).into(image);
+
+                txt.setText(response.body().getMeals().get(0).getIdMeal().toString());
+                txt2.setText(response.body().getMeals().get(0).getStrMeal());
+
+//////////////////////////////////////////////////////////////////////////////////
+
+            }
+
+            @Override
+            public void onFailure(Call<RootMeal> call, Throwable t) {
+
+            }
+        });
+
+
+        Retrofit apiClient2= APIClient.getClient();
+
+        Call<RootMeal> call2= apiInterface.getFilteredMeals("Egyptian");
+
+        call2.enqueue( new Callback<RootMeal>() {
+            @Override
+            public void onResponse(Call<RootMeal> call2, Response<RootMeal> response) {
+
+                if(response.isSuccessful()){
+
+                    ArrayList<Meal> meals = response.body().getMeals();
+
+
+                    recyclerView.setHasFixedSize(true);
+
+
+                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+
+                    recyclerView.setLayoutManager(linearLayoutManager);
+
+                    linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
+
+                    adapter = new MealAdapter(meals);
+
+                    recyclerView.setAdapter(adapter);
+
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RootMeal> call, Throwable t) {
+
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/foodplanner/MainActivity.java
+++ app/src/main/java/com/example/foodplanner/MainActivity.java
@@ -4,24 +4,3 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {}
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        RetrofitService.apiInterface.getIngredientsList("list").enqueue(new Callback<IngredientList>() {
-//            @Override
-//            public void onResponse(Call<IngredientList> call, Response<IngredientList> response) {
-//                if (response.isSuccessful()){
-//
-//                    Log.e("success", response.body().toString());
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<IngredientList> call, Throwable t) {
-//              Log.e("failure", t.)
-//            }
-//        });
-//    }
-//}
