e2610efa97c0890132426662c887bffeb21fc7da
eman <emankhaledsha@gmail.com>
2023-01-27 15:07:09 +0200

    Gradle and edit Login and edit xml Register
    Gradle and edit Login and edit xml Register

--- app/src/main/java/com/example/foodplanner/view/LogIn.java
+++ app/src/main/java/com/example/foodplanner/view/LogIn.java
@@ -1,27 +1,25 @@
 package com.example.foodplanner.view;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
 import com.example.foodplanner.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -42,13 +40,6 @@ public class LogIn extends Fragment {
 
     public LogIn() {
         // Required empty public constructor
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
     }
 
     @Override
@@ -134,3 +125,4 @@ public class LogIn extends Fragment {
 
 
 }
+
