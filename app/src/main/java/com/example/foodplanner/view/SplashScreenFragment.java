0bc0b969e5458af759df2473fa877ba12387ecc0
eman <emankhaledsha@gmail.com>
2023-02-02 13:41:15 +0200

    Edit to create Mvp

--- app/src/main/java/com/example/foodplanner/view/SplashScreenFragment.java
+++ app/src/main/java/com/example/foodplanner/view/SplashScreenFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -43,5 +44,14 @@ public class SplashScreenFragment extends Fragment {
             }
         });
 
+    }
+
+    public static class OnBoardingActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.onboarding);
+        }
     }
 }
